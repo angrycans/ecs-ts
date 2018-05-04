112180d9ebfe94d7c86ea6656000acaa303593cb
angrycans <angrycans@gmail.com>
2018-05-04 17:33:24 +0800

    init

---
+++ src/core/entity.ts
@@ -0,0 +1,24 @@
+import Component from './Component';
+
+class Entity {
+  private _isActive: boolean;
+  private _components: Component[];
+
+  constructor(...components: Component[]) {
+    this._components = components;
+  }
+
+  get isActive() {
+    return this._isActive;
+  }
+
+  set isActive(value: boolean) {
+    this._isActive = value;
+  }
+
+  get components() {
+    return this._components;
+  }
+}
+
+export default Entity;
